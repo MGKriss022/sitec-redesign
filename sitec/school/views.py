b2ed1eef6b34c9c6bdd0aabbcabed946d3aacbb6
Victor Ivan Diaz Dzib <lordbounced97@gmail.com>
2021-06-07 16:02:17 -0700

    Added login

--- sitec/school/views.py
+++ sitec/school/views.py
@@ -1,43 +1,43 @@
 from django.views.generic import TemplateView
 
 class LoginView(TemplateView):
-    template_name = 'sitec/login.html'
+    template_name = 'school/login.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
 
 class PanelView(TemplateView):
-    template_name = 'sitec/panel.html'
+    template_name = 'school/panel.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
 
 class SettingsView(TemplateView):
-    template_name = 'sitec/settings.html'
+    template_name = 'school/settings.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
         
 class ReinscriptionView(TemplateView):
-    template_name = 'sitec/reinscription.html'
+    template_name = 'school/reinscription.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)   
         
 class CycleAdvanceView(TemplateView):
-    template_name = 'sitec/cycle_advance.html'
+    template_name = 'school/cycle_advance.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)   
         
 class KardexView(TemplateView):
-    template_name = 'sitec/kardex.html'
+    template_name = 'school/kardex.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)   
         
 class LogView(TemplateView):
-    template_name = 'sitec/log.html'
+    template_name = 'school/log.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
